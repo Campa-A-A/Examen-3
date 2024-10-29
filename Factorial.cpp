63994d7a22cca9e91449f3b89fced6c6d0440bb3
Campa-A-A <xkanpa@gmail.com>
2024-10-29 11:34:54 -0600

    Se modifico el archivo del Factorial y se agrego la funcionalidad de cajero

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,5 +1,6 @@
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -14,55 +15,122 @@ int factorial(int n) {
     }
 }
 
-bool menu1()
+class Usuario
+{
+    private: 
+        float saldo;
+        float saldoAnterior;
+
+    public:
+        Usuario(float saldoInicial)
+        {
+            this->saldo = saldoInicial;
+        }
+
+        void consultar()
+        {
+            cout << endl << "Su saldo es: " << saldo;
+        }
+
+        void retirarEfectivo(float dineroRetirado)
+        {
+            if (saldo < dineroRetirado)
+            {
+                cout << endl << "No se puede retirar esa cantidad" << endl;
+                return;
+            }
+            saldoAnterior = saldo;
+            saldo -= dineroRetirado;
+            cout << endl << "Se retiro: " << dineroRetirado;
+        }
+
+        void saldoAntesDeRetiro()
+        {
+            cout << endl << "Saldo anterior: " << saldoAnterior << endl <<
+            "Saldo Actual: " << saldo;
+        }
+        
+        void menuCajero()
+        {
+            string opcion;
+            cout << endl << "Que desea hacer?" << endl <<
+            "Ver saldo :: 1 " << endl <<
+            "Retirar dinero :: 2" << endl << 
+            "Ver saldo antes de retiro :: 3" << endl <<
+            "Opcion: ";
+            cin>> opcion;
+
+            if (opcion == "1")
+            {
+                consultar();
+            }
+            else if (opcion == "2")
+            {
+                float dinero;
+
+                cout << endl << "Ingrese el dinero a retirar: ";
+                cin >>dinero;
+
+                retirarEfectivo(dinero);
+            }
+            else
+            {
+                saldoAntesDeRetiro();
+            }
+        }
+};
+
+int menuMain()
 {
     string opcionUsuario;
-    cout << endl
-    << "Desea calcular el factorial de un numero?" << endl 
-    << "Si --> y/Y" << endl  
-    << "No --> n/N" << endl 
-    << "Opcion: ";
+
+    backMenuMain:
+    cout << endl <<
+    "Que desea usar?" << endl <<
+    "Cajero automatico :: 1" << endl <<
+    "Calcular un factorial :: 2" << endl <<
+    "Salir del programa :: 3" << endl <<
+    "Ingreso opcion: ";
     cin >> opcionUsuario;
 
-    if (opcionUsuario != "y" && opcionUsuario != "Y" && opcionUsuario != "N" && opcionUsuario != "n")
+    if (opcionUsuario != "1" && opcionUsuario != "2" && opcionUsuario != "3")
     {
         cout << endl << "Ingreso invalido" << endl;
-        return true;
+        goto backMenuMain;
     }
-    return false;
+    
+    if (opcionUsuario == "1"){ return 1; }
+    if (opcionUsuario == "2"){ return 2; }
+    return 3;
 }
+
 
 int main()
 {
+    Usuario u1(20000);
     do
     {
-        back1:
-        string opcionUsuario;
-        cout << endl
-        << "Desea calcular el factorial de un numero?" << endl 
-        << "Si --> y/Y" << endl  
-        << "No --> n/N" << endl 
-        << "Opcion: ";
-        cin >> opcionUsuario;
+        int goku = menuMain();
 
-        if (opcionUsuario != "y" && opcionUsuario != "Y" && opcionUsuario != "N" && opcionUsuario != "n")
+        if (goku == 2) 
         {
-            cout << endl << "Ingreso invalido" << endl;
-            goto back1;
+            int numeroFactorial;
+
+            cout << "Ingrese el numero para calcular su factorial: ";
+            cin >> numeroFactorial;
+
+            cout << endl << "El factorial del numero " << numeroFactorial << " = " << factorial(numeroFactorial) << endl; 
         }
-
-        if( opcionUsuario == "N" && opcionUsuario == "N" )
+        else if ( goku == 1)
         {
-            return 0;
+            u1.menuCajero();
+        }
+        else
+        {
+            cout << endl << "Saliendo del programa" << endl;
+            break;
         }
         
-        int numeroFactorial;
-
-        cout << "Ingrese el numero para calcular su factorial: ";
-        cin >> numeroFactorial;
-
-        cout << endl << "El factorial del numero " << numeroFactorial << " = " << factorial(numeroFactorial) << endl; 
-
     } while (true);
     
     return 0;
